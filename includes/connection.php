ac05a742710647c61aed9ceea7ae076383aa1994
sashin-deemantha <114796882+Sashin517@users.noreply.github.com>
2026-08-12 01:55:40 +0530

    updated db

--- includes/connection.php
+++ includes/connection.php
@@ -8,8 +8,8 @@ class Database {
 
         if(!isset(Database::$connection)) {
 
-            Database::$connection = new mysqli("localhost", "root", "", "sapsri_admin_core");
-            //Database::$connection = new mysqli("localhost", "sapsril_sldevs", "wveeaj+!eTIUNHLI", "sapsril_sapsri_admin_core");
+            //Database::$connection = new mysqli("localhost", "root", "", "sapsri_admin_core");
+            Database::$connection = new mysqli("localhost", "sapsril_sldevs", "wveeaj+!eTIUNHLI", "sapsril_sapsri_admin_core");
     
 
             if (Database::$connection->connect_error) {
