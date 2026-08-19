cc22f9343d18d29127d463f63f0fe608e2e9cb6b
IshankaDinusha <166381146+IshankaDinusha@users.noreply.github.com>
2026-08-20 01:05:59 +0530

    fix the last commit with unfinnished line in admin-script.js causing an error.

--- admin/assets/js/admin-script.js
+++ admin/assets/js/admin-script.js
@@ -3459,10 +3459,6 @@ document.addEventListener("DOMContentLoaded", () => {
       limit: 10, // Default selected rows per page
     };
 
-    const viewModalEl = document.getElementById("viewModalEl");
-    if(viewModal) 
-
-
     let searchTimeout = null;
 
     window.loadPublications = function () {
